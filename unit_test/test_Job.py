4eb544b19f628184d0000c9f81d18f6dca77fdec
Yuming Yuan <yuanyumingzz@qq.com>
2023-02-11 20:47:41 +0800

    try to fix pytest

---
+++ unit_test/test_Job.py
@@ -0,0 +1,12 @@
+import pytest
+import job_distribution
+import numpy
+
+class Test:
+    def test_JobDist():
+        """
+        Purpose: 
+        """
+        nw_len_seq,nw_size_seq = job_distribution.generate_sequence_work()
+        print(nw_len_seq,nw_size_seq)
+    # end def
