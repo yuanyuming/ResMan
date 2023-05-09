18e1812c3aa88e4da2d8675303d9a37ba1623494
Yuan <yuanyumingzz@qq.com>
2023-05-09 09:01:59 +0000

    Auction untested

--- Auction.py
+++ Auction.py
@@ -4,5 +4,15 @@ import numpy as np
 
 
 class ReverseAuction:
-    def __init__(self) -> None:
+    def __init__(self, allocation_mechanism=AllocationMechanism.FirstPrice()) -> None:
+        self.allocation_mechanism = allocation_mechanism
         pass
+
+    def auction(self, bids=Machine.Bids()):
+        winner_machine, prices, second_prices = self.allocation_mechanism.allocate(bids)
+        if prices > bids.job.budget:
+            bids.job.pay = 0
+            return False
+        bids.job.pay = prices
+        winner_machine.allocate_job(bids.job)
+        return True
