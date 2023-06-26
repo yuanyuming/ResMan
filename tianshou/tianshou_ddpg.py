2fc8f9b8f03578f97d9ebaed273bc033cfe1a437
yuanyuming <yuanyumingzz@qq.com>
2023-06-26 16:08:33 +0800

    remove the useless file;update gitignore

---
+++ tianshou/tianshou_ddpg.py
@@ -0,0 +1,169 @@
+import argparse
+import os
+import pprint
+
+import gymnasium as gym
+import numpy as np
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+from tianshou.data import Collector, VectorReplayBuffer
+from tianshou.env import DummyVectorEnv
+from tianshou.exploration import GaussianNoise
+from tianshou.policy import DDPGPolicy
+from tianshou.trainer import offpolicy_trainer
+from tianshou.utils import TensorboardLogger
+from tianshou.utils.net.common import Net
+from tianshou.utils.net.continuous import Actor, Critic
+
+
+class Args:
+    def __init__(
+        self,
+        task="Pendulum-v1",
+        reward_threshold=None,
+        seed=0,
+        buffer_size=20000,
+        actor_lr=1e-4,
+        critic_lr=1e-3,
+        gamma=0.99,
+        tau=0.005,
+        exploration_noise=0.1,
+        epoch=5,
+        step_per_epoch=20000,
+        step_per_collect=8,
+        update_per_step=0.125,
+        batch_size=128,
+        hidden_sizes=[128, 128],
+        training_num=8,
+        test_num=100,
+        logdir="log",
+        render=0.0,
+        rew_norm=False,
+        n_step=3,
+        device="cuda" if torch.cuda.is_available() else "cpu",
+    ):
+        self.task = task
+        self.reward_threshold = reward_threshold
+        self.seed = seed
+        self.buffer_size = buffer_size
+        self.actor_lr = actor_lr
+        self.critic_lr = critic_lr
+        self.gamma = gamma
+        self.tau = tau
+        self.exploration_noise = exploration_noise
+        self.epoch = epoch
+        self.step_per_epoch = step_per_epoch
+        self.step_per_collect = step_per_collect
+        self.update_per_step = update_per_step
+        self.batch_size = batch_size
+        self.hidden_sizes = hidden_sizes
+        self.training_num = training_num
+        self.test_num = test_num
+        self.logdir = logdir
+        self.render = render
+        self.rew_norm = rew_norm
+        self.n_step = n_step
+        self.device = device
+
+
+def test_ddpg(args=Args()):
+    env = gym.make(args.task)
+    args.state_shape = env.observation_space.shape or env.observation_space.n
+    args.action_shape = env.action_space.shape or env.action_space.n
+    args.max_action = env.action_space.high[0]
+    if args.reward_threshold is None:
+        default_reward_threshold = {"Pendulum-v0": -250, "Pendulum-v1": -250}
+        args.reward_threshold = default_reward_threshold.get(
+            args.task, env.spec.reward_threshold
+        )
+    # you can also use tianshou.env.SubprocVectorEnv
+    # train_envs = gym.make(args.task)
+    train_envs = DummyVectorEnv(
+        [lambda: gym.make(args.task) for _ in range(args.training_num)]
+    )
+    # test_envs = gym.make(args.task)
+    test_envs = DummyVectorEnv(
+        [lambda: gym.make(args.task) for _ in range(args.test_num)]
+    )
+    # seed
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    train_envs.seed(args.seed)
+    test_envs.seed(args.seed)
+    # model
+    net = Net(args.state_shape, hidden_sizes=args.hidden_sizes, device=args.device)
+    actor = Actor(
+        net, args.action_shape, max_action=args.max_action, device=args.device
+    ).to(args.device)
+    actor_optim = torch.optim.Adam(actor.parameters(), lr=args.actor_lr)
+    net = Net(
+        args.state_shape,
+        args.action_shape,
+        hidden_sizes=args.hidden_sizes,
+        concat=True,
+        device=args.device,
+    )
+    critic = Critic(net, device=args.device).to(args.device)
+    critic_optim = torch.optim.Adam(critic.parameters(), lr=args.critic_lr)
+    policy = DDPGPolicy(
+        actor,
+        actor_optim,
+        critic,
+        critic_optim,
+        tau=args.tau,
+        gamma=args.gamma,
+        exploration_noise=GaussianNoise(sigma=args.exploration_noise),
+        reward_normalization=args.rew_norm,
+        estimation_step=args.n_step,
+        action_space=env.action_space,
+    )
+    # collector
+    train_collector = Collector(
+        policy,
+        train_envs,
+        VectorReplayBuffer(args.buffer_size, len(train_envs)),
+        exploration_noise=True,
+    )
+    test_collector = Collector(policy, test_envs)
+    # log
+    log_path = os.path.join(args.logdir, args.task, "ddpg")
+    writer = SummaryWriter(log_path)
+    logger = TensorboardLogger(writer)
+
+    def save_best_fn(policy):
+        torch.save(policy.state_dict(), os.path.join(log_path, "policy.pth"))
+
+    def stop_fn(mean_rewards):
+        return mean_rewards >= args.reward_threshold
+
+    # trainer
+    result = offpolicy_trainer(
+        policy,
+        train_collector,
+        test_collector,
+        args.epoch,
+        args.step_per_epoch,
+        args.step_per_collect,
+        args.test_num,
+        args.batch_size,
+        update_per_step=args.update_per_step,
+        stop_fn=stop_fn,
+        save_best_fn=save_best_fn,
+        logger=logger,
+    )
+    assert stop_fn(result["best_reward"])
+
+    if __name__ == "__main__":
+        pprint.pprint(result)
+        # Let's watch its performance!
+        env = gym.make(args.task)
+        policy.eval()
+        collector = Collector(policy, env)
+        result = collector.collect(n_episode=1, render=args.render)
+        rews, lens = result["rews"], result["lens"]
+        print(f"Final reward: {rews.mean()}, length: {lens.mean()}")
+
+
+if __name__ == "__main__":
+    test_ddpg()
